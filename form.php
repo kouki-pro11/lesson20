dbb1df661aed4432b30ca1e53bb84a425faa072b
test <example@example.com>
2020-10-04 15:22:35 +0900

    form.php up1

---
+++ form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>form</title>
+</head>
+<body>
+<h1>git study</h1>
+<p>一言入力</p>
+<form action="display.php" method="get">
+<input type="text" name="text">
+<button>送信する</button>
+</form>
+</body>
+</html>
