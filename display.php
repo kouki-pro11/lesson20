12b8dfe741a3af17c6982b3fbb6c86fcd5da7099
test <example@example.com>
2020-10-04 15:00:14 +0900

    display.php　up1

---
+++ display.php
@@ -0,0 +1,22 @@
+<?php
+if (empty($_GET['text'])) {
+	$text = '入力されていません';
+} else {
+	$text = $_GET['text'];
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>git_study</title>
+</head>
+<body>
+<h1>git_study</h1>
+<?php echo $text; ?>
+<?php if (!empty($_GET['text'])): ?>
+<p><a href="divination.php?num=<?php echo rand(1, 2); ?>">今日の運勢</a></p>
+<?php endif; ?>
+</body>
+</html>
