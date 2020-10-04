97898093ae38bd9d5406ed6c5e551aa35a7417d3
test <example@example.com>
2020-10-04 15:23:39 +0900

    divination.php up1

---
+++ divination.php
@@ -0,0 +1,26 @@
+<?php
+if (!isset($_GET['num'])) {
+	$ans =  '不正なアクセスです';
+} else {
+	$num = $_GET['num'];
+	switch($num) {
+		case '1':
+			$ans = '今日の運勢は大凶です';
+			break;
+		case '2':
+			$ans = '今日の運勢は大吉です';
+			break;
+	}
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>git_study</title>
+</head>
+<body>
+<?php echo $ans; ?>
+</body>
+</html>
